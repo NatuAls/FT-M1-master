3bf3545b0eef6fc8ffeab0f8a60fa2dd83943194
NatuAls <nahuelalesso19@gmail.com>
2022-08-04 15:58:26 -0300

    01-IntroToCS-homework

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,12 +2,29 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+  // opcion 1
+  let str = num.toString().split('').reverse().join('');
+  let suma = 0;
+  for(let i=0; i<str.length; i++){
+    suma += parseInt(str[i]) * Math.pow(2, i);
+  }
+  return suma;
+  // opcion 2
+  // return parseInt(num, 2)
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
+  // opcion 1
+  let resultado = '';
+  while(num >= 1){
+    resultado += Math.floor(num)%2;
+    num = num/2;
+  }
+  return resultado.split('').reverse().join('');
 
+  // opcion2
+  // return num.toString(2)
 }
 
 
