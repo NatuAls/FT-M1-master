212df2c752f132eced879aea118e37a9c761ccdc
NatuAls <nahuelalesso19@gmail.com>
2022-08-22 20:21:36 -0300

    homework06 complete

--- 06-EstructuraDeDatos-III/homework/homework.js
+++ 06-EstructuraDeDatos-III/homework/homework.js
@@ -10,9 +10,78 @@
 
   El ábrol utilizado para hacer los tests se encuentra representado en la imagen bst.png dentro del directorio homework.
 */
+class BinarySearchTree{
+  constructor(value){
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
 
-function BinarySearchTree() {}
+  size(){
+    if(!this.left && !this.right) return 1;
+    if(!this.left) return 1 + this.right.size();
+    if(!this.right) return 1 + this.left.size();
+    return 1 + this.left.size() + this.right.size();
+  }
 
+  insert(value){ 
+	if(value < this.value){
+		if(!this.left){
+			this.left = new BinarySearchTree(value);
+		}else{
+			this.left.insert(value);
+		}	
+	} else if(value > this.value){
+		if(!this.right){
+			this.right = new BinarySearchTree(value);
+		}else{
+			this.right.insert(value);
+		}	
+  }
+  }
+
+  contains(value){
+    if(value === this.value){
+      return true;
+    }
+    if(value < this.value && this.left){
+      return this.left.contains(value);
+    } else if(value > this.value && this.right){
+      return this.right.contains(value);
+    }
+    return false;
+  }
+
+  depthFirstForEach(cb, order){
+    if(order === 'post-order'){
+      if(this.left) this.left.depthFirstForEach(cb, order);
+      if(this.right) this.right.depthFirstForEach(cb, order);
+      cb(this.value);
+    } else if(order === 'pre-order'){
+      cb(this.value);
+      if(this.left) this.left.depthFirstForEach(cb, order);
+      if(this.right) this.right.depthFirstForEach(cb, order);
+    } else {
+     if(this.left) this.left.depthFirstForEach(cb, order);
+     cb(this.value);
+     if(this.right) this.right.depthFirstForEach(cb, order);
+    }
+  }
+
+  breadthFirstForEach(cb, queue = []){
+    //con if reducido:
+    cb(this.value);
+    if(this.left) queue.push(this.left);
+    if(this.right) queue.push(this.right);
+    if(queue.length) queue.shift().breadthFirstForEach(cb, queue);
+
+    //con operador ternario:
+    //cb(this.value);
+    //this.left ? arr.push(this.left) : 1;
+    //this.right ? arr.push(this.right) : 1;
+    //arr.length ? arr.shift().breadthFirstForEach(cb, arr) : 1;
+  }
+}
 // No modifiquen nada debajo de esta linea
 // --------------------------------
 
