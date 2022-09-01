54f35e66163b984156310f521be8dae69720e2b1
NatuAls <nahuelalesso19@gmail.com>
2022-09-01 16:37:36 -0300

    homework08 complete

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -6,7 +6,19 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  if(array.length <= 1) return array;
+  let pivot = array[0];
+  let max = [];
+  let min = [];
 
+  for (let i = 1; i < array.length; i++) {
+    if(array[i] > pivot){
+      max.push(array[i]);
+    } else {
+      min.push(array[i]);
+    }
+  }
+  return quickSort(min).concat(pivot, quickSort(max));
 }
 
 function mergeSort(array) {
@@ -14,7 +26,25 @@ function mergeSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  function merge(left, right){
+    let arr = [];
+    while(left.length && right.length){
+      if(left[0] < right[0]){
+        arr.push(left.shift())
+      } else {
+        arr.push(right.shift())
+      }
+    }
+    return arr.concat(left, right)
+  }
 
+  if(array.length === 1) return array;
+
+  let middle = Math.round(array.length / 2);
+  let left = array.slice(0, middle);
+  let right = array.slice(middle);
+  
+  return merge(mergeSort(left), mergeSort(right));
 }
 
 // No modificar nada debajo de esta línea
